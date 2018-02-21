226ec97ee8d8c8a386ace989a16a2c49530ebbb2
Domingo Perez <domingoperez.23@gmail.com>
2018-02-21 15:18:09 -0800

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(function(){
+    $('.flexslider').flexslider({
+    animation: "slide", //String: Select your animation type, "fade" or "slide"
+    controlNav: true //Boolean: Create navigation for paging control of each clide? Note: Leave true for manualControls usage
+  });
+});
